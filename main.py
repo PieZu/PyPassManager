6506c83ba7a6dd47b28d8334d81ff52a6a463df7
PieZu <amanofdabacc@gmail.com>
2020-06-12 03:10:21 +0000

    added prettier display

--- main.py
+++ main.py
@@ -11,19 +11,22 @@ salt = b"PyPassSalt73871" # unique salt to counteract lookup tables
 masterpass = hashlib.pbkdf2_hmac('sha512', masterpass, salt, 10000)
 
 # store how many refreshes used, to make sure the same password isnt used twice
-max_iterations = 1
+max_iterations = 0
 
 class Password:
-  def __init__(self, iteration="max", type="hex", crop=False):
+  def __init__(self, iteration="max", type="hex", crop=False, name="__default"):
 
     # by default the password will start off with the next iteration after the max (.refresh will bring it up by 1). For some reason setting this directly in the parameter definition doesn't work when you change max_iterations dynamically. Weird.
     if iteration == "max": 
       iteration = max_iterations
+    if name == "__default": # same thing with name wont work directly in the parameter
+      name = "Pass"+str(iteration+1)
 
     # store settings
     self.type = type
     self.crop_length = crop
     self.iteration = iteration
+    self.name = name
 
     # generate initial state
     self.refresh()
@@ -42,6 +45,9 @@ class Password:
 
     # generate the passwords hash / source  
     self.hash = hashlib.pbkdf2_hmac('sha256', masterpass, salt, iterations)
+
+    # store updated iteration
+    self.iteration = iterations
     
   def __repr__(self):
     # generate password plaintext with current settings
@@ -62,4 +68,11 @@ passwords = []
 for i in range(5):
   passwords.append(Password())
 
-print(passwords)
+def display():
+  print(" --- PyPassManager --- ")
+  for password in passwords:
+    print(f"[{password.iteration}] {password.name}: {password}")
+  print("")
+  input("type help for a list of commands\n")
+
+display()
